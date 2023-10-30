cf06d8b50ab3ce5752acf118c026ea2b01902fe4
gmurad97 <murad.dev@bk.ru>
2023-10-31 02:14:25 +0400

    header main finished added menu and add anim

---
+++ assets/js/script.js
@@ -0,0 +1,26 @@
+$(document).ready(function () {
+    /* SHOW NAVBARS MENU - START */
+    $("#navbar_menu_show").click(function () {
+        $(".navbar__menu").fadeIn();
+    });
+
+    $("#navbar_menu_close").click(function () {
+        $(".navbar__menu").fadeOut();
+    });
+    /* SHOW NAVBARS MENU - ENDED */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+});
