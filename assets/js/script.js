ea3a7cf133e447eab0432ee443fb700a42bc3991
gmurad97 <murad.dev@bk.ru>
2023-11-01 06:25:54 +0400

    main page finished(no rwd)

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,7 +10,7 @@ $(document).ready(function () {
     /* SHOW NAVBARS MENU - ENDED */
 
     /* ABOUT US DATA COUNTER - START */
-    $("[data-counter]").each(function(){
+    $("[data-counter]").each(function () {
         const $this = $(this);
         const countTo = $this.attr("data-counter");
         $this.counterUp({
@@ -19,4 +19,62 @@ $(document).ready(function () {
         });
     });
     /* ABOUT US DATA COUNTER - ENDED */
+
+    /* PARTNERS CAROUSEL - START */
+    $("#partners_carousel_i").owlCarousel({
+        items: 6,
+        loop: true,
+        margin: 10,
+        center: true,
+        nav: false,
+        dots: false,
+        autoplay: true,
+        autoplayTimeout: 2048,
+        smartSpeed: 512,
+        autoplayHoverPause: true,
+        /* responsiveClass: true,
+        responsive: {
+            0: {
+                items: 1,
+            },
+            600: {
+                items: 3,
+            },
+            768: {
+                items: 5,
+            },
+            1200: {
+                items: 6,
+            }
+        } */
+    });
+
+    $("#partners_carousel_ii").owlCarousel({
+        items: 6,
+        loop: true,
+        margin: 10,
+        center: false,
+        nav: false,
+        dots: false,
+        autoplay: true,
+        autoplayTimeout: 2048,
+        smartSpeed: 512,
+        autoplayHoverPause: true,
+        /* responsiveClass: true,
+        responsive: {
+            0: {
+                items: 1,
+            },
+            600: {
+                items: 3,
+            },
+            768: {
+                items: 5,
+            },
+            1200: {
+                items: 6,
+            }
+        } */
+    });
+    /* PARTNERS CAROUSEL - ENDED */
 });
